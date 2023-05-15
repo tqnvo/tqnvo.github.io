9f55233cd611348af630886ff7a4eba75d70899c
Tuan Vo <vo@acom.rwth-aachen.de>
2023-05-15 02:16:21 +0200

    up bio x31

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -2,21 +2,21 @@ var addressPoints = [
   [
     "Berkeley CA, USA",
     37.8708393,
-    -122.2728638
+    -122.272863
+  ],
+  [
+    "Aachen, Germany",
+    50.776351,
+    6.083862
+  ],
+  [
+    "Los Angeles, CA",
+    34.0536909,
+    -118.242766
   ],
   [
     "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Oxford University, Pembroke College, UK",
-    51.7511879,
-    -1.2573466
+    51.5073359,
+    -0.12765
   ]
 ];
